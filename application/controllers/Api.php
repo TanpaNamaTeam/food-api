d89a58ca93c11a5f7f529ba568de95a6f775f4b4
root <hasnan@tunnelbiz.com>
2017-04-30 08:47:22 +0800

    Update API v1.004

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -63,7 +63,7 @@ class Api extends CI_Controller {
             //if ( $apikey == $theapikey) {
             
             $uploaddir = './uploads/';
-            $file_name = underscore($_FILES['file']['name']);
+            $file_name = sha1($_FILES['file']['name'].date());
             $uploadfile = $uploaddir.$file_name;
 
             if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
